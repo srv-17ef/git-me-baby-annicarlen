b49b633ac3ee1b03d98fe29150396347e1853c4b
annicarlen <54536836+annicarlen@users.noreply.github.com>
2019-08-26 15:04:46 +0200

    Äntligen!!!!!!!

---
+++ phptest.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PHPtest</title>
+</head>
+<body>
+
+<?php
+echo '<h1>Kolla, php</h1><br>';
+
+$text = '<h2>Om nån :D</h2><br>';
+echo($text);
+
+$person = "Rickard";
+$age = "34";
+$size = "xxl";
+$nrOfKids = 2;
+$pets = "1 sköldpadda";
+$trueAge = 43.5;
+
+echo($person."<br>".$age."<br>".$size."<br>".$nrOfKids."<br>".$pets."<br>".$trueAge);
+
+?>
+
+</body>
+</html>

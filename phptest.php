e35a279aa52d9ce60c61123e67633b204dbc5429
annicarlen <54536836+annicarlen@users.noreply.github.com>
2019-08-28 10:37:16 +0200

    JAG GJORDE ELEFANTSONG

--- phptest.php
+++ phptest.php
@@ -16,13 +16,44 @@ $text = '<h2>Om nån :D</h2><br>';
 echo($text);
 
 $person = "Rickard";
-$age = "34";
+$age = "30";
 $size = "xxl";
 $nrOfKids = 2;
 $pets = "1 sköldpadda";
 $trueAge = 43.5;
 
-echo($person."<br>".$age."<br>".$size."<br>".$nrOfKids."<br>".$pets."<br>".$trueAge);
+echo($person."<br>".$size."<br>".$nrOfKids."<br>".$pets."<br>".$trueAge);
+
+if($trueAge > 40) {
+    echo('<br>GAMLING');
+}
+echo "<br>Ålder? ";
+
+switch( $age) {
+    case 10:
+        echo "10";
+       break;
+    case 20:
+        echo "20";
+        break;
+    case 30:
+        echo "30";
+        break;
+}
+for($i=1; $i<=100; $i++){
+    if($i===1){
+       echo "<br>En elefant balanserade
+på en liten spindel-tråd
+Det tyckte han var så intressant,
+så han gick och hämtade en annan elefant.";
+    } else {
+    echo "<br>$i elefanter balanserade
+på en liten spindel-tråd
+Det tyckte dom var så intressant,
+så dom gick och hämtade en annan elefant.";
+}}
+
+
 
 ?>
 
